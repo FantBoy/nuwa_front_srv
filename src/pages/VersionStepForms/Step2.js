85f6309b58149989da46201f8117fa23f610c4a5
BearboyXu <>
2019-04-17 22:21:00 +0800

    版本详情-文件列表空间重构

--- src/pages/VersionStepForms/Step2.js
+++ src/pages/VersionStepForms/Step2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "dva";
-import { Form, Input, Button, Alert, Divider } from "antd";
+import { Form, Input, Button, Alert, Divider, Table, Tag, InputNumber, Popconfirm } from "antd";
 import router from "umi/router";
 import { digitUppercase } from "@/utils/utils";
 import styles from "./style.less";
@@ -13,6 +13,194 @@ const formItemLayout = {
     span: 19
   }
 };
+
+const data = [];
+for (let i = 0; i < 30; i++) {
+  data.push({
+    key: i.toString(),
+    name: `Edrward ${i}`,
+    age: 32,
+    address: `London Park no. ${i}`,
+  });
+}
+
+const FormItem = Form.Item;
+const EditableContext = React.createContext();
+
+class EditableCell extends React.Component {
+  getInput = () => {
+    if (this.props.inputType === 'number') {
+      return <InputNumber />;
+    }
+    return <Input />;
+  };
+
+  render() {
+    const {
+      editing,
+      dataIndex,
+      title,
+      inputType,
+      record,
+      index,
+      ...restProps
+    } = this.props;
+    return (
+      <EditableContext.Consumer>
+        {(form) => {
+          const { getFieldDecorator } = form;
+          return (
+            <td {...restProps}>
+              {editing ? (
+                <FormItem style={{ margin: 0 }}>
+                  {getFieldDecorator(dataIndex, {
+                    rules: [{
+                      required: true,
+                      message: `Please Input ${title}!`,
+                    }],
+                    initialValue: record[dataIndex],
+                  })(this.getInput())}
+                </FormItem>
+              ) : restProps.children}
+            </td>
+          );
+        }}
+      </EditableContext.Consumer>
+    );
+  }
+}
+
+class EditableTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data, editingKey: '' };
+    this.columns = [
+      {
+        title: 'name',
+        dataIndex: 'name',
+        width: '25%',
+        editable: true,
+      },
+      {
+        title: 'age',
+        dataIndex: 'age',
+        width: '15%',
+        editable: true,
+      },
+      {
+        title: 'address',
+        dataIndex: 'address',
+        width: '40%',
+        editable: true,
+      },
+      {
+        title: 'operation',
+        dataIndex: 'operation',
+        render: (text, record) => {
+          const { editingKey } = this.state;
+          const editable = this.isEditing(record);
+          return (
+            <div>
+              {editable ? (
+                <span>
+                  <EditableContext.Consumer>
+                    {form => (
+                      <a
+                        href="javascript:;"
+                        onClick={() => this.save(form, record.key)}
+                        style={{ marginRight: 8 }}
+                      >
+                        Save
+                      </a>
+                    )}
+                  </EditableContext.Consumer>
+                  <Popconfirm
+                    title="Sure to cancel?"
+                    onConfirm={() => this.cancel(record.key)}
+                  >
+                    <a>Cancel</a>
+                  </Popconfirm>
+                </span>
+              ) : (
+                <a disabled={editingKey !== ''} onClick={() => this.edit(record.key)}>Edit</a>
+              )}
+            </div>
+          );
+        },
+      },
+    ];
+  }
+
+  isEditing = record => record.key === this.state.editingKey;
+
+  cancel = () => {
+    this.setState({ editingKey: '' });
+  };
+
+  save(form, key) {
+    form.validateFields((error, row) => {
+      if (error) {
+        return;
+      }
+      const newData = [...this.state.data];
+      const index = newData.findIndex(item => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row,
+        });
+        this.setState({ data: newData, editingKey: '' });
+      } else {
+        newData.push(row);
+        this.setState({ data: newData, editingKey: '' });
+      }
+    });
+  }
+
+  edit(key) {
+    this.setState({ editingKey: key });
+  }
+
+  render() {
+    const components = {
+      body: {
+        cell: EditableCell,
+      },
+    };
+
+    const columns = this.columns.map((col) => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          inputType: col.dataIndex === 'age' ? 'number' : 'text',
+          dataIndex: col.dataIndex,
+          title: col.title,
+          editing: this.isEditing(record),
+        }),
+      };
+    });
+
+    return (
+      <EditableContext.Provider value={this.props.form} >
+        <Table
+          components={components}
+          // bordered
+          dataSource={this.state.data}
+          columns={columns}
+          rowClassName="editable-row"
+          pagination={false}
+          size="small"
+        />
+      </EditableContext.Provider>
+    );
+  }
+}
+
 
 @connect(({ form, loading }) => ({
   submitting: loading.effects["form/submitStepForm"],
@@ -40,82 +228,28 @@ class Step2 extends React.PureComponent {
         }
       });
     };
+    const EditableFormTable = Form.create()(EditableTable);
+
     return (
-      <Form layout="horizontal" className={styles.stepForm}>
+      <div className={styles.stepFilesForm}>
         <Alert
           closable
           showIcon
-          message="确认转账后，资金将直接打入对方账户，无法退回。"
+          message="文件版本包，文件列表..."
           style={{ marginBottom: 24 }}
         />
-        <Form.Item
-          {...formItemLayout}
-          className={styles.stepFormText}
-          label="付款账户"
-        >
-          {data.payAccount}
-        </Form.Item>
-        <Form.Item
-          {...formItemLayout}
-          className={styles.stepFormText}
-          label="收款账户"
-        >
-          {data.receiverAccount}
-        </Form.Item>
-        <Form.Item
-          {...formItemLayout}
-          className={styles.stepFormText}
-          label="收款人姓名"
-        >
-          {data.receiverName}
-        </Form.Item>
-        <Form.Item
-          {...formItemLayout}
-          className={styles.stepFormText}
-          label="转账金额"
-        >
-          <span className={styles.money}>{data.amount}</span>
-          <span className={styles.uppercase}>
-            （{digitUppercase(data.amount)}）
-          </span>
-        </Form.Item>
-        <Divider style={{ margin: "24px 0" }} />
-        <Form.Item {...formItemLayout} label="支付密码" required={false}>
-          {getFieldDecorator("password", {
-            initialValue: "123456",
-            rules: [
-              {
-                required: true,
-                message: "需要支付密码才能进行支付"
-              }
-            ]
-          })(
-            <Input
-              type="password"
-              autoComplete="off"
-              style={{ width: "80%" }}
-            />
-          )}
-        </Form.Item>
-        <Form.Item
-          style={{ marginBottom: 8 }}
-          wrapperCol={{
-            xs: { span: 24, offset: 0 },
-            sm: {
-              span: formItemLayout.wrapperCol.span,
-              offset: formItemLayout.labelCol.span
-            }
-          }}
-          label=""
-        >
-          <Button type="primary" onClick={onValidateForm} loading={submitting}>
+        
+        <EditableFormTable />
+        <Button type="primary" onClick={onValidateForm} loading={submitting} className={styles.stepBtn}>
             提交
           </Button>
-          <Button onClick={onPrev} style={{ marginLeft: 8 }}>
+          <Button onClick={onPrev} style={{ marginLeft: 8 }} className={styles.stepBtn}>
             上一步
           </Button>
-        </Form.Item>
-      </Form>
+        
+      </div>
+      
+      
     );
   }
 }
