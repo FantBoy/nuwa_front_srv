ac6c2e8579ffb5ba5217ededd9eaced5e5083d38
BearboyXu <>
2019-04-17 21:56:04 +0800

    版本详情-文件列表样式

--- src/pages/VersionStepForms/Step1.js
+++ src/pages/VersionStepForms/Step1.js
@@ -30,63 +30,33 @@ class Step1 extends React.PureComponent {
             type: "form/saveStepFormData",
             payload: values
           });
-          router.push("/form/step-form/confirm");
+          router.push("/packagetube/version/step-form/confirm");
         }
       });
     };
     return (
       <Fragment>
         <Form layout="horizontal" className={styles.stepForm} hideRequiredMark>
-          <Form.Item {...formItemLayout} label="付款账户">
-            {getFieldDecorator("payAccount", {
-              initialValue: data.payAccount,
-              rules: [{ required: true, message: "请选择付款账户" }]
-            })(
-              <Select placeholder="test@example.com">
-                <Option value="ant-design@alipay.com">
-                  ant-design@alipay.com
-                </Option>
-              </Select>
-            )}
+          <Form.Item {...formItemLayout} label="包名">
+            {getFieldDecorator("packageName", {
+              initialValue: data.packageName,
+              rules: [{ required: true, message: "请输入包名" }]
+            })(<Input placeholder="请输入包名" />)}
           </Form.Item>
-          <Form.Item {...formItemLayout} label="收款账户">
-            <Input.Group compact>
-              <Select defaultValue="alipay" style={{ width: 100 }}>
-                <Option value="alipay">支付宝</Option>
-                <Option value="bank">银行账户</Option>
-              </Select>
-              {getFieldDecorator("receiverAccount", {
-                initialValue: data.receiverAccount,
-                rules: [
-                  { required: true, message: "请输入收款人账户" },
-                  { type: "email", message: "账户名应为邮箱格式" }
-                ]
-              })(
-                <Input
-                  style={{ width: "calc(100% - 100px)" }}
-                  placeholder="test@example.com"
-                />
-              )}
-            </Input.Group>
+          <Form.Item {...formItemLayout} label="版本号">
+            {getFieldDecorator("packageVersion", {
+              initialValue: data.packageVersion,
+              rules: [{ required: true, message: "请输入版本号" }]
+            })(<Input placeholder="请输入版本号" />)}
           </Form.Item>
-          <Form.Item {...formItemLayout} label="收款人姓名">
-            {getFieldDecorator("receiverName", {
-              initialValue: data.receiverName,
-              rules: [{ required: true, message: "请输入收款人姓名" }]
-            })(<Input placeholder="请输入收款人姓名" />)}
+       
+          <Form.Item {...formItemLayout} label="版本描述">
+            {getFieldDecorator("versionDesc", {
+              initialValue: data.versionDesc,
+              rules: [{ required: true, message: "请输入版本描述" }]
+            })(<Input placeholder="请输入版本描述" />)}
           </Form.Item>
-          <Form.Item {...formItemLayout} label="转账金额">
-            {getFieldDecorator("amount", {
-              initialValue: data.amount,
-              rules: [
-                { required: true, message: "请输入转账金额" },
-                {
-                  pattern: /^(\d+)((?:\.\d+)?)$/,
-                  message: "请输入合法金额数字"
-                }
-              ]
-            })(<Input prefix="￥" placeholder="请输入金额" />)}
-          </Form.Item>
+          
           <Form.Item
             wrapperCol={{
               xs: { span: 24, offset: 0 },
